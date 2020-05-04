f3e062c9e96fa68ec3b871798d355911ab369023
Michael Ritter <info@michi-ritter.de>
2020-05-04 15:58:46 +0200

    Update benchmark_section_4.sh

--- benchmark_section_4.sh
+++ benchmark_section_4.sh
@@ -4,7 +4,9 @@
 echo "Section 4.1: Disable Bonjour advertising service"
 echo "------------------------------------------------------------------------"
 echo "Output:"
-defaults read /Library/Preferences/com.apple.alf globalstate
+echo "==========================================="
+defaults read /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements
+echo "==========================================="
 echo "\nNotes:"
 echo "Verify the value returned is 1 or 2"
 echo "Perform the following to implement the prescribed state:"
@@ -16,11 +18,23 @@ echo "\n"
 echo "Section 4.2: Check Wi-Fi status in menu bar"
 echo "------------------------------------------------------------------------"
 echo "Output:"
+echo "==========================================="
 defaults read com.apple.systemuiserver menuExtras | grep AirPort.menu
+echo "==========================================="
 echo "\nNotes:"
-echo "Verify the value returned is: /System/Library/CoreServices/Menu Extras/AirPort.menu"
+echo "Verify the value returned is: /System/Library/CoreServices/Menu/Extras/AirPort.menu"
 echo "Perform the following to implement the prescribed state:"
 echo "Edit \"Show Wi-Fi\" in System Preferences > Network"
+echo "------------------------------------------------------------------------"
+echo "\n"
+
+# Section 4.3: Wifi status menu bar
+echo "Section 4.3: Create network specific locations (Not Scored)"
+echo "------------------------------------------------------------------------"
+echo "Output:"
+echo "==========================================="
+echo "Manual Audit"
+echo "==========================================="
 echo "------------------------------------------------------------------------"
 echo "\n"
 
@@ -28,7 +42,9 @@ echo "\n"
 echo "Section 4.4: Audit http server (Should not be running)"
 echo "------------------------------------------------------------------------"
 echo "Output:"
+echo "==========================================="
 ps -ef | grep -i httpd
+echo "==========================================="
 echo "\nNotes:"
 echo "There should be no results for /usr/sbin/httpd"
 echo "Perform the following to implement the prescribed state:"
@@ -37,29 +53,21 @@ echo "\"sudo defaults write /System/Library/LaunchDaemons/org.apache.httpd Disab
 echo "------------------------------------------------------------------------"
 echo "\n"
 
-# Section 4.5: Audit ftp server (should not be running)
-echo "Section 4.5: Audit ftp server (should not be running)"
-echo "------------------------------------------------------------------------"
-echo "Output:"
-sudo launchctl list | egrep ftp
-echo "\nNotes:"
-echo "There should be no results for com.apple.ftpd"
-echo "Perform the following to implement the prescribed state:"
-echo "\"sudo -s launchctl unload -w /System/Library/LaunchDaemons/ftp.plist\""
-echo "------------------------------------------------------------------------"
-echo "\n"
 
-# Section 4.6: Audit nfs server (should not be running)
-echo "Section 4.6: Audit nfs server (should not be running)"
+# Section 4.5:  Ensure nfs server is not running
+echo "Section 4.5:  Ensure nfs server is not running"
 echo "------------------------------------------------------------------------"
 echo "Output:"
+echo "==========================================="
 ps -ef | grep -i nfsd
 echo "There should be no results for /sbin/nfsd"
 cat /etc/exports
 echo "Should return \"No such file or directory\""
+echo "==========================================="
 echo "\nNotes:"
 echo "Perform the following to implement the prescribed state:"
 echo "\"sudo nfsd disable\""
 echo "\"rm /etc/export\""
 echo "------------------------------------------------------------------------"
 echo "\n"
+
